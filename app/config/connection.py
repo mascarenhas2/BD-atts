a1c637b8bc7d8c5a472396a288447a81dedb667c
mascarenhas2 <160731737+mascarenhas2@users.noreply.github.com>
2024-10-21 12:31:53 +0000

    estudos

--- app/config/connection.py
+++ app/config/connection.py
@@ -10,3 +10,19 @@ db_port = "3306"
 db_name = "meu_banco"
 # URL de conexão para BD MySQL . 
 DATABASE_URL = f"mysql+pymsql://(db_user):(db_password)@(db_host):(db_port)/(db_name)"
+
+#Conectando ao banco de dados.
+Session = sessionmaker(bind=db)
+session = Session()
+
+@contextmanager
+def get_db():
+    db = Session()
+    try:
+        yield db # Caso a sessão realize todas as tarefas, salva a operação.
+        db.commit()
+    except Exception as erro:
+        db.rollback() # Desfaz todas as alterações em caso de erro em alguma operação.
+        raise erro # Lanca uma exception.
+    finally:
+        db.close() # Fecha conexão com banco de dados.
